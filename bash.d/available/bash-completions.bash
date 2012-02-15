57fa7385a579f1bd44886ae5248df85754a4cb4e
Jonathan Grochowski <jongrocho@gmail.com>
2012-02-15 06:08:06 -0800

    [Minor] Fix bash-completion sourcing

--- bash.d/available/bash-completions.bash
+++ bash.d/available/bash-completions.bash
@@ -1,5 +1,5 @@
 # Source all *-bash-completion files under ~/.bash-completion.d/enabled.
-BASH_COMPLETION_USER_DIR=~/.bash.completion.d/enabled
+BASH_COMPLETION_USER_DIR=~/.bash-completion.d/enabled
 if [[ -d ${BASH_COMPLETION_USER_DIR} && -r ${BASH_COMPLETION_USER_DIR} && \
   -x ${BASH_COMPLETION_USER_DIR} ]]; then
     for f in ${BASH_COMPLETION_USER_DIR}/*-bash-completion ; do
