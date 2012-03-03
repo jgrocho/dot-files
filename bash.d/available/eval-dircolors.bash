f053cc7fb958bb617d426c0e5935e7447b1f0feb
Jonathan Grochowski <jongrocho@gmail.com>
2012-03-02 18:12:06 -0800

    Generalize dircolors location

--- bash.d/available/eval-dircolors.bash
+++ bash.d/available/eval-dircolors.bash
@@ -1,3 +1,5 @@
 # Install some nice colors for ls.
-[[ -x /usr/bin/dircolors && -f ~/.dircolors && -r ~/.dircolors ]] \
-  && eval `/usr/bin/dircolors ~/.dircolors`
+__dircolors=`type -p dircolors`
+[[ -x "${__dircolors}" && -f ~/.dircolors && -r ~/.dircolors ]] \
+  && eval `${__dircolors} ~/.dircolors`
+unset __dircolors
