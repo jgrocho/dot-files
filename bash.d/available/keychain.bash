3feb4f193f1a12f0f6a22c79f43673aeccadb596
Jonathan Grochowski <jongrocho@gmail.com>
2015-01-07 03:51:18 -0800

    Make keychain arguments explicit

--- bash.d/available/keychain.bash
+++ bash.d/available/keychain.bash
@@ -1,5 +1,5 @@
 # Load the ssh keys into memory
 if hash keychain 2>/dev/null; then
-  eval $(keychain --eval -Q -q --noask)
+  eval $(keychain --agents ssh,gpg --eval --quiet --quick --noask)
   alias ssh='keychain -Q -q --ignore-missing id_ecdsa id_rsa; ssh'
 fi
